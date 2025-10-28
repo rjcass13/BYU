5fb67dafe2771e526c4303ecf8b88a45948d557c
rjcass13 <48258924+rjcass13@users.noreply.github.com>
2025-10-27 22:02:58 -0600

    Delete stat536/HW4 directory

--- stat536/HW4/EDA.R
+++
@@ -1,5 +0,0 @@
-mark <- read.table("TargetedMarketing.csv", sep = ';', header = TRUE, stringsAsFactors = TRUE)
-
-# Set all unknown or non-existant data to 'NA' throughout table
-mark$pdays[which(mark$pdays == 999)] <- NA
-# mean(mark$pdays, na.rm = TRUE)
