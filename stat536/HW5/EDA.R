614ed25523382df0b33133b319e0817de2cdcc52
rjcass13 <rjcass13@gmail.com>
2025-11-07 10:49:34 -0700

    HW5 (excluding csv)

---
+++ stat536/HW5/EDA.R
@@ -0,0 +1,38 @@
+library(mgcv)
+cc <- read.csv("CCFraud.csv", stringsAsFactors = TRUE)
+cc <- cc[, -1] # Get rid of the 'X' column which is just the row indices
+
+# Sort of Monotonic: within the heavy region on the right it is
+plot(cc$V1,jitter(cc$Class), main = 'Fraudulent vs. V1', xlab = 'V1', ylab = 'Fraudulent (yes/no)')
+smooth_fit <- gam(cc$Class ~ s(cc$V1))
+lines(cc$V1[order(cc$V1)], predict(smooth_fit)[order(cc$V1)], col = "red", lwd = 2)
+
+# Monotonic
+plot(cc$V2,jitter(cc$Class), main = 'Fraudulent vs. V2', xlab = 'V2', ylab = 'Fraudulent (yes/no)')
+smooth_fit <- gam(cc$Class ~ s(cc$V2))
+lines(cc$V2[order(cc$V2)], predict(smooth_fit)[order(cc$V2)], col = "red", lwd = 2)
+
+# Monotonic
+plot(cc$V3,jitter(cc$Class), main = 'Fraudulent vs. V3', xlab = 'V3', ylab = 'Fraudulent (yes/no)')
+smooth_fit <- gam(cc$Class ~ s(cc$V3))
+lines(cc$V3[order(cc$V3)], predict(smooth_fit)[order(cc$V3)], col = "red", lwd = 2)
+
+# Monotonic
+plot(cc$V4,jitter(cc$Class), main = 'Fraudulent vs. V4', xlab = 'V4', ylab = 'Fraudulent (yes/no)')
+smooth_fit <- gam(cc$Class ~ s(cc$V4))
+lines(cc$V4[order(cc$V4)], predict(smooth_fit)[order(cc$V4)], col = "red", lwd = 2)
+
+# Once there are rejections, yes, it's monotonic
+plot(cc$V5,jitter(cc$Class), main = 'Fraudulent vs. V5', xlab = 'V5', ylab = 'Fraudulent (yes/no)')
+smooth_fit <- gam(cc$Class ~ s(cc$V5))
+lines(cc$V5[order(cc$V5)], predict(smooth_fit)[order(cc$V5)], col = "red", lwd = 2)
+
+# Iffy
+plot(cc$V6,jitter(cc$Class), main = 'Fraudulent vs. V6', xlab = 'V6', ylab = 'Fraudulent (yes/no)')
+smooth_fit <- gam(cc$Class ~ s(cc$V6))
+lines(cc$V6[order(cc$V6)], predict(smooth_fit)[order(cc$V6)], col = "red", lwd = 2)
+
+# Monotonic
+plot(cc$V7,jitter(cc$Class), main = 'Fraudulent vs. V7', xlab = 'V7', ylab = 'Fraudulent (yes/no)')
+smooth_fit <- gam(cc$Class ~ s(cc$V7))
+lines(cc$V7[order(cc$V7)], predict(smooth_fit)[order(cc$V7)], col = "red", lwd = 2)
