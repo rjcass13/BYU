51b9a478bc60518ca7ec96f86fa2d4db328e5239
rjcass13 <rjcass13@gmail.com>
2025-11-11 15:11:11 -0700

    in class

--- stat536/Unit5/EDA.R
+++ stat536/Unit5/EDA.R
@@ -18,39 +18,77 @@ plot(low_accept$CGPA, low_accept$Research)
 
 ## JDF hacks for plotting cool stuff
 dim = 101
-gre_seq = seq(290,340,length.out = dim)
-gpa_seq = seq(min(adm$CGPA),4,length.out = dim)
-uni_seq = seq(1,5,length.out = dim)
-res_seq = seq(0,1,length.out = dim)
-gre_test = rep(gre_seq,dim)
-gpa_test = rep(gpa_seq,each=dim)
-uni_test = rep(uni_seq,dim)
-res_test = rep(res_seq,dim)
-X.test = cbind(gre_test,gpa_test, res_test, uni_test)
-points(X.test,col=4,cex=.2)
-X.test.df = data.frame(CGPA = gpa_test, GRE.Score = gre_test, Research = res_test, University.Rating = uni_test)
+gre_seq = seq(290, 340, length.out = dim)
+gpa_seq = seq(min(adm$CGPA), 4, length.out = dim)
+uni_seq = seq(1, 5, length.out = dim)
+res_seq = seq(0, 1, length.out = dim)
+sop_seq = seq(1, 5, length.out = dim)
+lor_seq = seq(1, 5, length.out = dim)
+gre_test = rep(gre_seq, dim)
+gpa_test = rep(gpa_seq, dim)
+uni_test = rep(uni_seq, dim)
+res_test = rep(res_seq, dim)
+sop_test = rep(sop_seq, dim)
+lor_test = rep(lor_seq, dim)
+X_test = cbind(gre_test, gpa_test, res_test, uni_test, sop_test, lor_test)
+#points(X_test,col=6,cex=.2)
+X_test_df = data.frame(CGPA = gpa_test, GRE.Score = gre_test, Research = res_test, University.Rating = uni_test, SOP = sop_test, LOR = lor_test)
 
 
 #################################
 ########### KNN
 #################################
 # X <- fastDummies::dummy_cols(ad[,1:6])  # faster than model.matrix, but doesn't like this dataset because there are no categorical variables
-X.train = model.matrix(Status ~ GRE.Score + CGPA + Research + University.Rating, data = adm)
-X.combined = rbind(X.train[,-1],X.test)
+X_train = model.matrix(Status ~ GRE.Score + CGPA + Research + University.Rating, data = adm)
+X_combined = rbind(X_train[,-1], X_test)
 # indices of train, test
-train = 1:nrow(X.train)
-test = (nrow(X.train)+1):nrow(X.combined)
+train = 1:nrow(X_train)
+test = (nrow(X_train)+1):nrow(X_combined)
 
 # Center and Scale (standardize)
-X.scaled <- scale(X.combined)
+X_scaled <- scale(X.combined)
 
 library(class)
-model1 = knn(train = X.scaled[train,], test=X.scaled[test,], cl = adm$Status, k = 3)
+model1 = knn(train = X_scaled[train,], test=X_scaled[test,], cl = adm$Status, k = 3)
 plot(gre_test, gpa_test, col=as.numeric(model1),pch=19)
 
 
 # Decorrelate
-XX <- X.scaled%*%solve(chol(cov(X.scaled)))
+XX <- X_scaled%*%solve(chol(cov(X_scaled)))
 
 model2 = knn(train = XX[train,], test=XX[test,], cl = adm$Status, k = 3)
 plot(gre_test, gpa_test, col=as.numeric(model2), pch=19)
+
+
+
+#################################
+########### Bagging
+#################################
+library(ranger)
+library(vip)
+rf <- ranger(Status ~ ., data=adm, num.trees = 200
+  # , mtry = 4
+  , importance = "impurity", probability = T)  
+p = predict(rf, data = X_test_df)
+plot_these = which(p$predictions[,1] < .6 & p$predictions[,1] > .4)
+plot(adm$GRE.Score, adm$CGPA, col = as.numeric(adm$Status) + 1, pch=16)
+points(X_test_df$GRE.Score[plot_these], X_test_df$CGPA[plot_these], col=5, pch=20)
+# Different Importance outputs
+importance(rf)
+vip(rf, num_features = 20)
+
+#################################
+########### Boosting
+#################################
+library(gbm)
+boost <- gbm(Status ~ CGPA + GRE.Score, data=adm
+             # , distribution = "bernoulli"
+             # , n.trees = 100
+             )
+# yhat.boost = predict(boost,newdata = ??)
+summary(boost)
+p = predict(boost, newdata = X_test_df,type='response')
+plot_these = which(p<.6 & p>.4)
+plot(adm$GRE.Score, adm$CGPA, col = as.numeric(adm$Status) + 1, pch=16)
+points(gre_test[plot_these], gpa_test[plot_these], col=5, pch=20)
+
